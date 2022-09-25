f51603914def3e0cf55adb706c2f2b7d443d89f3
Artem Marchenko <art.marchenko@gmail.com>
2022-09-25 20:33:59 +0300

    init commit

---
+++ src/main/java/com/logos/swagerdemo/SwagerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.logos.swagerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwagerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwagerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/logos/swagerdemo/SwagerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logos.swagerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwagerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
